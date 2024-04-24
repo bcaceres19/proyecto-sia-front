e784474584d66bdaebe56da2c5f336625d947932
bcaceres <bacg20044@gmail.com>
2024-04-24 03:24:20 -0500

    Se activa guardian

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   {
     path:'administrador',
     component: MenuComponent,
-   // canActivate: [permisosGuard],
+   canActivate: [permisosGuard],
     children: [
       {
         path: '',
